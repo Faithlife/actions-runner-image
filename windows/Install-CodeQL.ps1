7f31f876bba4bd98c61ab80e321ea728a4a2700a
Bradley Grainger <bgrainger@gmail.com>
2023-08-31 18:31:33 -0700

    Write the .complete file for the tool cache.

--- windows/Install-CodeQL.ps1
+++ windows/Install-CodeQL.ps1
@@ -9,8 +9,11 @@ Invoke-WebRequest -Uri $($codeql_release.assets | Where-Object { $_.name -eq "co
 
 # extract it to the correct location in the runner tool cache
 $codeql_version = $codeql_release.html_url.replace('https://github.com/github/codeql-action/releases/tag/codeql-bundle-v', '')
-$codeql_path = "C:\actions-runner\_work\_tool\CodeQL\$codeql_version\x64\"
+$codeql_path = "C:\actions-runner\_work\_tool\CodeQL\$codeql_version\x64"
 New-Item -ItemType Directory -Force -Path $codeql_path > $null
 tar -xzf C:\codeql.tar.gz -C $codeql_path
 
 Remove-Item "c:\codeql.tar.gz"
+
+# write a .complete file so that the tool cache knows that the tool is installed; https://github.com/actions/toolkit/blob/3d652d3133965f63309e4b2e1c8852cdbdcb3833/packages/tool-cache/src/tool-cache.ts#L528
+New-Item -ItemType File -Path "$codeql_path.complete" -Force > $null
